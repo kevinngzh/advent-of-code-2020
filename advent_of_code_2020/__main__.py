19ff2b3f1cf1922bd0aacac2f15145641157c469
Kevin Ng <kevin@ngzhk.net>
2020-12-01 21:55:21 +0800

    Default to the most recent available day if the day is not supplied

--- advent_of_code_2020/__main__.py
+++ advent_of_code_2020/__main__.py
@@ -2,6 +2,7 @@
 Advent of Code 2020 solutions.
 
 Usage:
+    advent_of_code_2020 <input_file>
     advent_of_code_2020 day <day> <input_file>
     advent_of_code_2020 -h | --help
     advent_of_code_2020 --version
@@ -15,17 +16,32 @@ Options:
 __version__ = "0.1.5"
 
 
+import re
 import sys
 
 from docopt import docopt
-from schema import Schema, Use
+from schema import Schema, Or, Use
+
+
+def _get_latest_day():
+    pattern = re.compile("day(\d+)$")
+    days = []
+
+    for path in os.listdir("advent_of_code_2020"):
+        match = pattern.search(path)
+
+        if match is not None:
+            days.append(int(match.group(1)))
+
+    return max(days)
 
 
 if __name__ == "__main__":
     import importlib
+    import os
 
     s = Schema({
-        "<day>": Use(int),
+        "<day>": Or(None, Use(int)),
         "<input_file>": Use(open),
     }, ignore_extra_keys=True)
 
@@ -33,7 +49,8 @@ if __name__ == "__main__":
 
     entries = [int(i) for i in args["<input_file>"].readlines()]
 
-    day = importlib.import_module("day{:02d}".format(args["<day>"]))
+    day = args["<day>"] or _get_latest_day()
+    day = importlib.import_module("day{:02d}".format(day))
 
     print(day.part1(entries))
     print(day.part2(entries))
