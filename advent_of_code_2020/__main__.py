3710e7d65347d0fc04acbd335341c219dbb11b57
Kevin Ng <kevin@ngzhk.net>
2020-12-01 18:57:17 +0800

    Use docopt to parse command line arguments

--- advent_of_code_2020/__main__.py
+++ advent_of_code_2020/__main__.py
@@ -1,19 +1,36 @@
-__version__ = "0.1.2"
+"""
+Advent of Code 2020 solutions.
+
+Usage:
+    advent_of_code_2020 <input_file>
+    advent_of_code_2020 -h | --help
+    advent_of_code_2020 --version
+
+Options:
+    -h --help   Show this screen.
+    --version   Show version.
+"""
+
+
+__version__ = "0.1.3"
 
 
 import sys
+
+from docopt import docopt
+from schema import Schema, Use
 
 
 if __name__ == "__main__":
     from day01 import example, part1, part2
 
-    if len(args := sys.argv[1:]) > 0:
-        filepath = sys.argv[1]
+    s = Schema({
+        "<input_file>": Use(open),
+    }, ignore_extra_keys=True)
 
-        with open(filepath) as f:
-            entries = [int(i) for i in f.readlines()]
-    else:
-        entries = example
+    args = s.validate(docopt(__doc__, version=__version__))
+
+    entries = [int(i) for i in args["<input_file>"].readlines()]
 
     print(part1(entries))
     print(part2(entries))
