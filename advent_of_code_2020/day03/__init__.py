4789af8ce3daffbfe50dd0f3d400271853e499c4
Kevin Ng <kevin@ngzhk.net>
2020-12-03 13:28:02 +0800

    Generalise tree-counting function

--- advent_of_code_2020/day03/__init__.py
+++ advent_of_code_2020/day03/__init__.py
@@ -16,7 +16,7 @@ def parse_input(input_):
     return grid
 
 
-def part1(grid):
+def get_trees_encountered(grid, slope):
     # Start at the top-left corner.
     current_pos = [0, 0]
     max_x = max(x for x, y in grid.keys())
@@ -25,11 +25,15 @@ def part1(grid):
     trees_count = 0
 
     while current_pos[1] <= (max_y - 1):
-        current_pos = (current_pos[0] + 3) % (max_x + 1), current_pos[1] + 1
+        current_pos = (current_pos[0] + slope[0]) % (max_x + 1), current_pos[1] + slope[1]
 
         trees_count += grid[current_pos]
 
     return trees_count
+
+
+def part1(grid):
+    return get_trees_encountered(grid, (3, 1))
 
 
 def part2(entries):
