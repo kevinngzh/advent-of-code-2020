caac975da63aca6ba3b1146b3823acc8813afc3d
Kevin Ng <kevin@ngzhk.net>
2020-12-05 13:50:39 +0800

    Cleanup

--- advent_of_code_2020/__main__.py
+++ advent_of_code_2020/__main__.py
@@ -13,7 +13,7 @@ Options:
 """
 
 
-__version__ = "0.4.1"
+__version__ = "0.5.1"
 
 
 import re
--- advent_of_code_2020/day05/test_day05.py
+++ advent_of_code_2020/day05/test_day05.py
@@ -6,7 +6,3 @@ def test_part1():
     assert generate_seat_id("BFFFBBFRRR")[1:] == (70, 7, 567)
     assert generate_seat_id("FFFBBBFRRR")[1:] == (14, 7, 119)
     assert generate_seat_id("BBFFBBFRLL")[1:] == (102, 4, 820)
-
-
-def test_part2():
-    pass
