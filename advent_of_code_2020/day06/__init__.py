b9e6a3742b7de1b56267b3698d80e5a86456a0ae
Kevin Ng <kevin@ngzhk.net>
2020-12-07 11:31:04 +0800

    Solution for day 6 part 2

--- advent_of_code_2020/day06/__init__.py
+++ advent_of_code_2020/day06/__init__.py
@@ -8,6 +8,17 @@ class GroupAnswer:
     def all_answers(self):
         return set(answer for answers in self.person_answers for answer in answers)
 
+    @property
+    def yes_answers(self):
+        yeses = self.all_answers
+
+        for answers in self.person_answers:
+            for answer in list(yeses):
+                if answer not in answers:
+                    yeses.remove(answer)
+
+        return yeses
+
 
 def parse_input(input_):
     return [GroupAnswer(group_answer.split("\n")) for group_answer in input_.split("\n\n")]
@@ -17,5 +28,5 @@ def part1(group_answers):
     return sum(len(group.all_answers) for group in group_answers)
 
 
-def part2(entries):
-    pass
+def part2(group_answers):
+    return sum(len(group.yes_answers) for group in group_answers)
--- advent_of_code_2020/day06/test_day06.py
+++ advent_of_code_2020/day06/test_day06.py
@@ -40,5 +40,15 @@ def test_part1(example):
     assert result == 11
 
 
+def test_group_only_yes_answer(example):
+    assert len(example[0].yes_answers) == 3
+    assert len(example[1].yes_answers) == 0
+    assert len(example[2].yes_answers) == 1
+    assert len(example[3].yes_answers) == 1
+    assert len(example[4].yes_answers) == 1
+
+
 def test_part2(example):
-    pass
+    result = part2(example)
+
+    assert result == 6
