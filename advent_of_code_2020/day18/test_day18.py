06df5cb88f2494b3a047f51d1c1ce1531de52003
Kevin Ng <kevin@ngzhk.net>
2020-12-18 13:54:42 +0800

    Set up tests for part 2

--- advent_of_code_2020/day18/test_day18.py
+++ advent_of_code_2020/day18/test_day18.py
@@ -1,18 +1,23 @@
 import pytest
 
-from .__init__ import evaluate_new_math
+from .__init__ import evaluate_new_math, evaluate_new_advanced_math
 
 
 examples = [
-    ("1 + 2 * 3 + 4 * 5 + 6", 71),
-    ("1 + (2 * 3) + (4 * (5 + 6))", 51),
-    ("2 * 3 + (4 * 5)", 26),
-    ("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437),
-    ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240),
-    ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632),
+    ("1 + 2 * 3 + 4 * 5 + 6", (71, 231)),
+    ("1 + (2 * 3) + (4 * (5 + 6))", (51, 51)),
+    ("2 * 3 + (4 * 5)", (26, 46)),
+    ("5 + (8 * 3 + 9 + 3 * 4 * 3)", (437, 1445)),
+    ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", (12240, 669060)),
+    ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", (13632, 23340)),
 ]
 
 
 @pytest.mark.parametrize("raw_example,expected", examples)
 def test_evaluate_new_math(raw_example, expected):
-    assert evaluate_new_math(raw_example) == expected
+    assert evaluate_new_math(raw_example) == expected[0]
+
+
+@pytest.mark.parametrize("raw_example,expected", examples)
+def test_evaluate_new_advanced_math(raw_example, expected):
+    assert evaluate_new_advanced_math(raw_example) == expected[1]
